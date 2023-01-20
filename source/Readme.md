e4a7e5a8ee7f894be998f4a8adfdfeeaab47c7af
MariusR <mrochon@microsoft.com>
2023-01-20 13:59:53 -0800

    Added Reflector

---
+++ source/Readme.md
@@ -0,0 +1,4 @@
+# Reflector
+
+A simple web site that can receive GET or POST requestest and will output their
+details to log.LogInformation. It can be viewed in portal monitor.
---
+++ source/Reflector/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reflector.Models;
+using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+
+namespace Reflector.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [HttpPost]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<IActionResult> Index()
+        {
+            _logger.LogInformation("=============New request===================");
+            //await HttpResponseWritingExtensions.WriteAsync(HttpContext.Response, $"<ul>");
+            foreach (var h in Request.Headers)
+            {
+                _logger.LogInformation($"{h.Key} ---> {h.Value}");
+            }
+            var rdr = new StreamReader(Request.Body);
+            var body = await rdr.ReadToEndAsync();
+            _logger.LogInformation($"BODY: {body}");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
